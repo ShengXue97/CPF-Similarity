6c6ed31e0b21dc08eb54189e290bf6cc7d480cad
ShengXue97 <simshengxue1997@gmail.com>
2020-03-19 16:34:02 +0800

    change to bert

--- app.py
+++ app.py
@@ -1,64 +1,93 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# ##### Copyright 2018 The TensorFlow Hub Authors.
+# # Sentence Similarity with Pretrained BERT
+# In this notebook, we use a pretrained [BERT model](https://arxiv.org/abs/1810.04805) as a sentence encoder to measure sentence similarity. We use a [feature extractor](../../utils_nlp/bert/extract_features.py) that wraps [Hugging Face's PyTorch implementation](https://github.com/huggingface/pytorch-pretrained-BERT) of Google's [BERT](https://github.com/google-research/bert). 
 # 
-# Licensed under the Apache License, Version 2.0 (the "License");
-#
-# Copyright 2018 The TensorFlow Hub Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
+# **Note: To learn how to do pre-training on your own, please reference the [AzureML-BERT repo](https://github.com/microsoft/AzureML-BERT) created by Microsoft.**
 
-from absl import logging
+# ### 00 Global Settings
 
-import tensorflow as tf
-import tensorflow_hub as hub
-import matplotlib.pyplot as plt
-import numpy as np
+# In[2]:
+
+
+import sys
 import os
+import torch
+import itertools
+import numpy as np
 import pandas as pd
-import re
-import seaborn as sns
-from flask import Flask, request, abort, jsonify
-import requests
+import scrapbook as sb
+from collections import OrderedDict
 
-module_url = "https://tfhub.dev/google/universal-sentence-encoder/2" #@param ["https://tfhub.dev/google/universal-sentence-encoder/2", "https://tfhub.dev/google/universal-sentence-encoder-large/3"]
+sys.path.append("../../")
+from utils_nlp.models.bert.common import Language, Tokenizer
+from utils_nlp.models.bert.sequence_encoding import BERTSentenceEncoder, PoolingStrategy
 
-# Import the Universal Sentence Encoder's TF Hub module
-embed = hub.Module(module_url)
 
-def get_correlation(labels, features, rotation):
-  corr = np.inner(features, features)
-  return corr[0][1]
+# In[3]:
 
-def run_and_get_correlation(session_, input_tensor_, messages_, encoding_tensor):
-  message_embeddings_ = session_.run(
-      encoding_tensor, feed_dict={input_tensor_: messages_})
-  return get_correlation(messages_, message_embeddings_, 90)
 
-def get_similarity(input1, input2):
-  messages = []
-  messages.append(input1)
-  messages.append(input2)
+# device config
+NUM_GPUS = 0
 
-  similarity_input_placeholder = tf.placeholder(tf.string, shape=(None))
-  similarity_message_encodings = embed(similarity_input_placeholder)
-  with tf.Session() as session:
-    session.run(tf.global_variables_initializer())
-    session.run(tf.tables_initializer())
-    return run_and_get_correlation(session, similarity_input_placeholder, messages,
-                similarity_message_encodings)
+# model config
+LANGUAGE = Language.ENGLISH
+TO_LOWER = True
+MAX_SEQ_LENGTH = 128
+LAYER_INDEX = -2
+POOLING_STRATEGY = PoolingStrategy.MEAN
+
+# path config
+CACHE_DIR = "./temp"
+
+
+# In[4]:
+
+
+if not os.path.exists(CACHE_DIR):
+    os.makedirs(CACHE_DIR, exist_ok=True)
+
+
+# ### 01 Define the Sentence Encoder with Pretrained BERT
+
+# The `BERTSentenceEncoder` defaults to Pretrained BERT.
+
+# In[ ]:
+
+
+se = BERTSentenceEncoder(
+    language=LANGUAGE,
+    num_gpus=NUM_GPUS,
+    cache_dir=CACHE_DIR,
+    to_lower=TO_LOWER,
+    max_len=MAX_SEQ_LENGTH,
+    layer_index=LAYER_INDEX,
+    pooling_strategy=POOLING_STRATEGY,
+)
+
+
+# ### 02 Compute the Sentence Encodings
+
+# The `encode` method of the sentence encoder accepts a list of text to encode, as well as the layers we want to extract the embeddings from and the pooling strategy we want to use. The embedding size is 768. We can also return just the values column as a list of numpy arrays by setting the `as_numpy` parameter to True.
+
+# In[ ]:
+
+
+result = se.encode(
+    ["Coffee is good", "The moose is across the street"],
+    as_numpy=False
+)
+result
+
+
+# In[ ]:
+
+
+# for testing
+size_emb = len(result["values"].iloc[0])
+sb.glue("size_emb", size_emb)
+
 
 app = Flask(__name__)
 
